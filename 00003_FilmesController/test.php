167a9e51d84538842a30efdc4372cef3d75ee0bf
ssiqueira@digitalhouse.com <ssiqueira@digitalhouse.com>
2019-08-06 14:13:16 +0000

    Mumuki Export on 2019-08-06 14:13:16 +0000

--- 00003_FilmesController/test.php
+++ 00003_FilmesController/test.php
@@ -5,13 +5,13 @@ public function testPeliculaListado(): void {
 
 		$pc = new FilmesController();
 		
-		$this->assertTrue(method_exists($pc, 'listar'), "Falta o método <em>listar</em> dentro de FilmesController");
+		$this->assertTrue(method_exists($pc, 'listar'), "Falta o método listar dentro de FilmesController");
 		
 		$resul = $pc->listar();
 		
 		$this->assertTrue($pasePorView, "Você não utilizou a função view");
 		
-		$this->assertTrue($resul === "listarFilmes", "O método <em>listar</em> deve redirecionar para a view listarFilmes. Esqueceu um return? Se não, verifique o nome da view retornada");
+		$this->assertTrue($resul === "listarFilmes", "O método listar deve redirecionar para a view listarFilmes. Esqueceu um return? Se não, verifique o nome da view retornada");
 	}
 
 	public function testPeliculasDetalle(): void {
@@ -21,7 +21,7 @@ public function testPeliculaListado(): void {
 
 		$pc = new FilmesController();
 		
-		$this->assertTrue(method_exists($pc, 'detalhes'), "Falta o método <em>detalhes</em> dentro de FilmesController");
+		$this->assertTrue(method_exists($pc, 'detalhes'), "Falta o método detalhes dentro de FilmesController");
 		
 		$r = new ReflectionMethod("FilmesController", "detalhes");
 		$params = $r->getParameters();
@@ -31,10 +31,10 @@ public function testPeliculaListado(): void {
 		try {
 			$resul = $pc->detalhes(3);
 		} catch (Exception $e) {
-			$this->assertTrue(false, "O método <em>detalhes</em> deveria passar para a view uma (e somente uma) variável");
+			$this->assertTrue(false, "O método detalhes deveria passar para a view uma (e somente uma) variável");
 		}
 		
-		$this->assertTrue($pasePorView, "Parece que você não utilizou a função <em>view</em>");
+		$this->assertTrue($pasePorView, "Parece que você não utilizou a função view");
 		
-		$this->assertTrue($resul === "detalhesFilme3", "O método detalhes deve redirecionar para a view <em>detalhesFilme</em> e deve passar o id como parâmetro para essa view. Não esqueça o return para retornar a view.");
+		$this->assertTrue($resul === "detalhesFilme3", "O método detalhes deve redirecionar para a view detalhesFilme e deve passar o id como parâmetro para essa view. Não esqueça o return para retornar a view.");
 	}
