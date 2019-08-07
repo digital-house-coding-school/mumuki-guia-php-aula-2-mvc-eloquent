76bff953e7f42c0deb23522afd25597141c6e208
thomaz@digitalhouse.com <thomaz@digitalhouse.com>
2019-08-07 20:59:38 +0000

    Mumuki Export on 2019-08-07 20:59:38 +0000

--- 00009_Filtro e Ordem/test.php
+++ 00009_Filtro e Ordem/test.php
@@ -1,7 +1,7 @@
 public function testFiltroEOrdem(): void {
-    global $passouPelaView;
+    global $pasePorView;
     
-    $passouPelaView = false;
+    $pasePorView = false;
     
     $pc = new FilmesController();
     
@@ -11,7 +11,7 @@ public function testFiltroEOrdem(): void {
       $this->assertTrue(false, $e->getMessage());
     }
     
-    $this->assertTrue($passouPelaView, "Você está chamando a função view?");
+    $this->assertTrue($pasePorView, "Você está chamando a função view?");
     
     $this->assertTrue(is_string($resul), "Você está retornando o resultado da função view? Verifique o nome da view que está retornando.");
   }
