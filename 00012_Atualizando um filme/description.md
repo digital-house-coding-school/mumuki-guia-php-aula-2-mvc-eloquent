8a9ab489e2530410d437a03335725255f2171610
ssiqueira@digitalhouse.com <ssiqueira@digitalhouse.com>
2019-08-07 21:51:44 +0000

    Mumuki Export on 2019-08-07 21:51:44 +0000

--- 00012_Atualizando um filme/description.md
+++ 00012_Atualizando um filme/description.md
@@ -1,1 +1,26 @@
-.
+**Esclarecimento: Neste exercício não é necessário que você adicione a linha `use App\Movie;`. Você pode assumir que o arquivo já está incluído **
+
+E agora... vamos atualizar um filme!
+
+Para isso, você deve criar uma função chamada `update` que receberá o seguinte formulário pelo POST para atualizar um filme que **já estava no banco de dados**:
+
+``` html
+<form action = "/filmes/update" method = "POST">
+  {{csrf_field}}
+  <input type = "text" name = "title">
+  <input type = "text" name = "rating">
+  <input type = "text" name = "awards">
+</ form>
+```
+
+Esta função de armazenamento deve:
+
+> 1. Receber **dois** parâmetros. O primeiro do tipo `Request`. O segundo será o id do filme para atualizar.
+
+> 2. Obtenha um objeto do tipo `Filme` usando o método `find` e o id recebido como parâmetro.
+
+> 3. Passe os atributos atualizados ao objeto `Filme` baseado nos dados da`Request`. Felizmente, o banco de dados tem os mesmos nomes de coluna que os campos `<input>` no formulário
+
+> 4. Chame o método save
+
+> 5. Redirecione o usuário para a URL **/filmes/listar ** através da função `redirect`
--- 00012_Atualizando um filme/test.php
+++ 00012_Atualizando um filme/test.php
@@ -1,3 +1,48 @@
-public function testDescriptionExample(): void {
-  $this->assertTrue(true);
-}
+public function testUpdate(): void {
+		global $pasePorRedirect;
+		global $request;
+		global $pasePorSave;
+		
+		$request = new Request();
+		
+		$this->assertTrue(method_exists("FilmesController",'update'),"Falta o método update na FilmesController");
+		
+		$r = new ReflectionMethod("FilmesController", "update");
+		$params = $r->getParameters();
+		
+		$this->assertTrue(count($params) === 2, "O método update deve receber dois parâmetros");
+		
+		$this->assertTrue($params[0]->getType() !== null && $params[0]->getType()->getName() === "Request", "O primeiro parãmetro recebido pelo método deve ser do tipo Request");
+		
+		$pasePorRedirect = false;
+		
+		$pc = new PeliculasController();
+		
+		$request->title = "O Rei Leão";
+		$request->rating = 9.2;
+		$request->awards = 5;
+		$request->secret = 1;
+		
+		try {
+		  $resul = $pc->actualizar($request, 1);
+		} catch(Exception $e) {
+		  $this->assertTrue(false, $e->getMessage());
+		}
+		
+		$request->title = "Wall-e";
+		$request->rating = 8.1;
+		$request->awards = 4;
+		$request->secret = 2;
+		
+		try {
+		  $resul = $pc->actualizar($request, 2);
+		} catch(Exception $e) {
+		  $this->assertTrue(false, $e->getMessage());
+		}
+		
+		$this->assertTrue($pasePorSave, "Mmm... parece que não está armazenando nada.");
+		
+		$this->assertTrue($pasePorRedirect, "Lembre de chamar a função redirect");
+		
+		$this->assertTrue(is_string($resul), "Você está retornando o resultado da função redirect?");
+	  }
