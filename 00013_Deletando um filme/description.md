59f842e042477f75b92c2c151d2db2fb8469cff5
ssiqueira@digitalhouse.com <ssiqueira@digitalhouse.com>
2019-08-07 22:45:15 +0000

    Mumuki Export on 2019-08-07 22:45:15 +0000

--- 00013_Deletando um filme/description.md
+++ 00013_Deletando um filme/description.md
@@ -1,1 +1,23 @@
-.
+**Esclarecimento: Neste exercício não é necessário que você adicione a linha `use App\Movie;`. Você pode assumir que o arquivo já está incluído**
+
+E chegamos ao final da aula, onde finalmente iremos deletar um filme.
+
+Para isso, você deve criar uma função chamada `delete` que receberá o seguinte formulário por DELETE para saber o **id** de qual filme deletar:
+
+``` html
+<form action = "/ movies / remove" method = "POST">
+   {{csrf_field}}
+   <input type="hidden" name="id" valor="{{$filme->id}}"
+   <input type="hidden" name="_method" value="DELETE">
+</ form>
+```
+
+Esta função `delete` deve:
+
+> 1. Receber um parâmetro do tipo Request
+
+> 2. Obtenha um objeto do tipo `Filme` usando o método` find` e o id recebido no objeto `Request`
+
+> 3. Chame o método `delete`
+
+> 4. Redirecionar o usuário para o URL **/filmes/listar** através da função `redirect`
--- 00013_Deletando um filme/test.php
+++ 00013_Deletando um filme/test.php
@@ -1,3 +1,46 @@
-public function testDescriptionExample(): void {
-  $this->assertTrue(true);
-}
+public function testDelete(): void {
+		global $pasePorRedirect;
+		global $request;
+		global $pasePorDelete;
+		global $id;
+		
+		$request = new Request();
+		
+		$this->assertTrue(method_exists("FilmesController",'delete'),"Falta o método delete na classe FilmesController");
+		
+		$r = new ReflectionMethod("FilmesController", "delete");
+		$params = $r->getParameters();
+		
+		$this->assertTrue(count($params) === 1, "O método delete deve receber um parâmetro.");
+		
+		$this->assertTrue($params[0]->getType() !== null && $params[0]->getType()->getName() === "Request", "O parâmetro recebido pelo delete deve ser do tipo request");
+		
+		$pasePorRedirect = false;
+		
+		$pc = new FilmesController();
+		
+		$request->id = 2;
+		$id = 2;
+		
+		try {
+		  $resul = $pc->delete($request);
+		} catch(Exception $e) {
+		  $this->assertTrue(false, $e->getMessage());
+		}
+		
+		$request->id = 3;
+		$id = 3;
+		
+		try {
+		  $resul = $pc->delete($request);
+		} catch(Exception $e) {
+		  $this->assertTrue(false, $e->getMessage());
+		}
+		
+		$this->assertTrue($pasePorDelete, "Mmm...parece que você não está deletando nada.");
+		
+		$this->assertTrue($pasePorRedirect, "Você deveria chamar a função redirect!");
+		
+		$this->assertTrue(is_string($resul), "Verifique se você está retornando o resultado da função redirect.");
+		
+	}
